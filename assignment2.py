4ea290e1f04d2cf6943725378090e8ee7c79f18f
Connor Hanwick <chanwick@gmail.com>
2021-09-11 15:10:17 -0400

    created second_approach branch

--- assignment2.py
+++ assignment2.py
@@ -1,7 +1,7 @@
 import argparse
 import urllib.request
 import logging
-import datetime
+from datetime import datetime
 # from pprint import pprint
 
 def downloadData(url):
@@ -11,23 +11,42 @@ def downloadData(url):
         #print(data)
         return data
 
+
 # Write a function called ​processData​, which takes the contents of the file as the first parameter, processes 
 # the file line by line, and returns a dictionary that maps a person’s ID to a tuple of the form (name, birthday). 
 def processData(file_content):
-    a_dict = {}           
-            
+    person_data = {}                
     a_list = file_content.splitlines()
+#takes data from url, decodes it from utf to str, stores entries in a list separated by commas—then puts them into a dictionar
 
-#takes data from url, decodes it from utf to str, stores entries in a list separated by commas—then puts them into a dictionary
     for a in a_list:
         a = a.decode("utf-8")
         b = a.split(",")
-        a_dict[b[0]] = (b[1], b[2])
+        try:
+            # print(a)
+            format = "%m/%d/%Y" 
+            date_req_format = datetime.strptime(b[2], format).date()   
+            person_data[b[0]] = (b[1], date_req_format)
+            # print(b[0], b[1], date_req_format, b[2])
+        except:
+            pass
+            #print("bad format")
+
+
 
 # GOTTA CONVERT THE BDAY IN THE TUPLE TO A  DATETIME OBJECT
 
+    # for value in person_data.values():
+    #     try:
+    #         format = "%m/%d/%Y"
+    #         req_format = datetime.strptime(value[1], format).date()
+    #         print(req_format)
+    #     except:
+    #         pass
+    
 # return dictionary  
-    for key, value in a_dict.items():
+#UNCOMMENT
+    for key, value in person_data.items():
         print(key, ":", value)
         
         
--- datetimeformat.py
+++ datetimeformat.py
@@ -1,1 +1,32 @@
-#sketch
+# #sketch
+# #dd/mm/yyyy
+
+
+from datetime import datetime
+import urllib.request
+
+
+
+# bday = "09/10/1986"
+# format = "%m/%d/%Y"
+# req_format = datetime.strptime(bday, format).date()
+
+# print(req_format)
+
+
+bdays = ["12/24/2002", "6/20/2021", "03/02/2022"]
+bdays_new = []
+
+for b in bdays:
+    format = "%m/%d/%Y"
+    req_format = datetime.strptime(b, format).date()
+    bdays_new.append(req_format)
+    print(type(req_format))
+
+print(bdays_new)
+
+##On why datetime objects print weird from lists/dicts: https://stackoverflow.com/questions/311627/how-to-print-a-date-in-a-regular-format
+
+
+# for key, value in a_dict.items():
+#     print(key, type(value))
