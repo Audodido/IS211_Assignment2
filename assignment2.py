fe9b0c39206702a3a46c74907a34477eb8f6010c
Connor Hanwick <chanwick@gmail.com>
2021-09-12 11:44:52 -0400

    Finished main function. Assignment Finished-ish

--- assignment2.py
+++ assignment2.py
@@ -1,16 +1,20 @@
-
 import argparse
 import urllib.request
 import logging
 from datetime import datetime
-# from pprint import pprint
+
+
 
 def downloadData(url):
     """Downloads the data"""
-    with urllib.request.urlopen(url) as response:
-        data = response.read()
-        #print(data)
-        return data
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
+            return data
+    except Exception as error:
+        print("Uh Oh. Double check the url you entered and try again.")
+        exit()
 
 
 def processData(file_content):
@@ -26,42 +30,56 @@ def processData(file_content):
         b = a.split(",") # splits each entry into it's own list
         try:
             date_req_format = datetime.strptime(b[2], format).date() #formats datetime obj
-            person_data[b[0]] = (b[1], date_req_format)
+            person_data[b[0]] = (b[1], date_req_format) #adds entries to dictionary
         except ValueError:
-            LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
-            logging.basicConfig(filename = "assignment2.txt",
-                    level = logging.DEBUG,
-                    format = LOG_FORMAT,
-                    filemode = "w")
-            logger = logging.getLogger()
             logging.error("Error processing line #{} for ID #{}".format(index, b[0]))
 
         index += 1
 
+    
+    return person_data #returns dictionary
 
-
-    # return dictionary  
-    for key, value in person_data.items():
-        print(key, ":", value) # return not print
         
         
+def displayPerson(id, personData):
+    """Gets name and bday of a user"""
+    try:
+        name, date = personData[str(id)]
+        print("Person #{} is {} with a birthday of {}".format(id, name, date)) 
+    except:
+        print("No user found with that ID")
 
-# def displayPerson(id, personData):
-#     pass
+
+def main(url):
+    print(f"Running main with URL = {url}...")
+
+    LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
+    logging.getLogger("assignment2")
+    logging.basicConfig(filename="error.log", level=logging.ERROR, format=LOG_FORMAT, filemode="w")
+
+    csvData = downloadData(url)
+    personData = processData(csvData)
 
 
-# def main(url):
-#     print(f"Running main with URL = {url}...")
+    # prompts user for ID input until they give a value of <= 0
+    val = True
+
+    while val > 0:
+        val = int(input("Enter a user ID: "))
+        if val < 1:
+            exit()
+        else:
+            displayPerson(val, personData)
+
+
 
 
 if __name__ == "__main__":
-    processData(downloadData("https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv"))
-
-    # """Main entry point"""
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
-    # args = parser.parse_args()
-    # main(args.url)
+    """Main entry point"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
+    args = parser.parse_args()
+    main(args.url)
 
 
 
